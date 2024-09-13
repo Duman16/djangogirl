3942e2671a86adae793c2b6cbbd516f78eac7eb9
Duman <duman29042006@gmail.com>
2024-09-13 12:11:11 +0500

    gg

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -25,19 +27,25 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    
+    if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+            # Убедитесь, что пользователь аутентифицирован
+            if request.user.is_authenticated:
+                post.author = request.user  # Назначаем автором аутентифицированного пользователя
+                post.save()
+                return redirect('post_detail', post_id=post.pk)
+            else:
+                return HttpResponseForbidden("You must be logged in to edit this post.")
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+
+    return render(request, 'edit_post.html', {'form': form})
 
 
 
